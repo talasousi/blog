99012f1dbbaeccb4bf291c0ae6a3ad583d5c929a
talasousi <talasousi@me.com>
2017-07-24 19:43:15 +0300

    second commit

--- blog/urls.py
+++ blog/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('posts.urls')),
-    url(r'^', include('stories.urls')),
+    url(r'^posts/', include('posts.urls', namespace="posts")),
+    url(r'^stories/', include('stories.urls')),
     ]
 
--- posts/models.py
+++ posts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 # Create your models here.
 class Post(models.Model):
@@ -10,3 +11,5 @@ class Post(models.Model):
 	def __str__(self):
 		return self.title
 
+	def get_absolute_url(self):
+		return reverse("posts:detail", kwargs={"post_id": self.id})

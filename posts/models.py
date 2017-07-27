064db5a22119fcb68c212e584e6a8ef0b2f37c63
talasousi <talasousi@me.com>
2017-07-27 17:15:34 +0300

    added images

--- posts/models.py
+++ posts/models.py
@@ -4,6 +4,7 @@ from django.core.urlresolvers import reverse
 # Create your models here.
 class Post(models.Model):
 	title = models.CharField(max_length=50)
+	image = models.ImageField(upload_to="blog_images", null=True, blank=True)
 	content = models.TextField()
 	updated = models.DateTimeField(auto_now=True)
 	timestamp = models.DateTimeField(auto_now_add=True)
--- posts/views.py
+++ posts/views.py
@@ -4,6 +4,7 @@ from .models import Post
 from django.shortcuts import get_object_or_404
 from .forms import PostForm
 from django.contrib	import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
 
 #def post_create(request):
@@ -17,7 +18,7 @@ from django.contrib	import messages
 
 def post_update(request, post_id):
 	post_object = get_object_or_404(Post, id=post_id)
-	form = PostForm(request.POST or None, instance=post_object)
+	form = PostForm(request.POST or None, request.FILES or None, instance=post_object)
 	if form.is_valid():
 		form.save()
 		messages.success(request, "Post updated")
@@ -35,12 +36,25 @@ def post_delete(request, post_id):
 	return redirect("posts:list")
 
 def post_list(request):
-	obj_list = Post.objects.all()#.order_by("-timestamp", "-updated")
-	context = {
-		"post_list": obj_list,
-	}
+    obj_list = Post.objects.all()
 
-	return render(request, 'post_list.html', context)
+    paginator = Paginator(obj_list, 4) # Show 25 contacts per page
+
+    page = request.GET.get('page')
+    try:
+        objs = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        objs = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        objs = paginator.page(paginator.num_pages)
+
+    context = {
+    "post_list": objs,
+    }
+
+    return render(request, 'post_list.html', context)
 
 def post_detail(request, post_id):
 	obj = get_object_or_404(Post, id=post_id)
@@ -50,7 +64,7 @@ def post_detail(request, post_id):
 	return render(request, 'post_detail.html', context)
 
 def post_create(request):
-	form = PostForm(request.POST or None)
+	form = PostForm(request.POST or None, request.FILES or None)
 	if form.is_valid():
 		form.save()
 		messages.success(request, "Post created")
